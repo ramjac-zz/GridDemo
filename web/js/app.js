b91238de0a50b4c8161dedb72b06dc69dced4a10
Grady Fields <ramjacvp@gmail.com>
2017-08-16 23:09:40 -0400

    Added background color toggle.

--- web/js/app.js
+++ web/js/app.js
@@ -1,5 +1,34 @@
+window.onload = function () {
+    // toggle area colors
+    var showBackgroundColors = false;
+    document.getElementById('background-colors').addEventListener('click', function () {
+        showBackgroundColors = !showBackgroundColors;
+        if (showBackgroundColors) {
+            var cnt = 0;
+            for(var element of  document.getElementsByClassName('grid-box')){
+                element.style.backgroundColor = getColor(cnt);
+                cnt++;
+            }
+        } else {
+            for(var element of  document.getElementsByClassName('grid-box')){
+                element.style.backgroundColor = "white";
+            }
+        }
+    });
 
-// toggle area colors
+    // toggle grid line visibility
+    var showGridLines = false;
+    document.getElementById('grid-lines').addEventListener('click', function () {
+        showGridLines = !showGridLines;
+        if (showGridLines) {
 
-// toggle grid line visibility
+        } else {
 
+        }
+    });
+};
+
+function getColor(cnt){
+    colors=["antiquewhite", "aquamarine", "aliceblue", "greenyellow","lavender", "lightgreen"];
+    return colors[cnt% colors.length];
+}
