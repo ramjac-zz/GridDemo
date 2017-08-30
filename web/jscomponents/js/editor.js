12492626aacd261f672981f0f3e9535ea8f1a025
Grady Fields <ramjacvp@gmail.com>
2017-08-29 22:10:14 -0400

    Tweaking the JS components.
    Making the Vuejs component save to localStorage.

--- web/jscomponents/js/editor.js
+++ web/jscomponents/js/editor.js
@@ -1,16 +1,37 @@
 new Vue({
-    el: '#editor',
-    data: {
-      input: '# hello css grid'
-    },
-    computed: {
-      compiledMarkdown: function () {
-        return marked(this.input, { sanitize: true })
-      }
-    },
-    methods: {
-      update: _.debounce(function (e) {
-        this.input = e.target.value
-      }, 300)
+  el: '#editor',
+  data: {
+    //input: window.localStorage.getItem("file1") 
+    input: '# hello css grid',
+    bouncy: '# hello css grid',
+    data_title: 'temporary name'
+  },
+  computed: {
+    compiledMarkdown: function () {
+      return marked(this.input, {
+        sanitize: true
+      })
     }
-  })
+  },
+  methods: {
+    getFileName: function (name) {
+      var strReg = new RegExp('[A-Z a-z \s]+');
+      // get the new name
+      var tmpName = strReg.exec(this.input)[0];
+      if (tmpName.length > 7) {
+        tmpName = tmpName.substr(0, 7).trim();
+      }
+
+      if (name !== tmpName) {
+        window.localStorage.setItem(tmpName, this.input);
+        window.localStorage.removeItem(name);
+        name = tmpName;
+      }
+      return name;
+    },
+    update: _.debounce(function (e) {
+      this.input = this.bouncy;
+      this.data_title = this.getFileName(this.data_title);
+    }, 300)
+  }
+})
